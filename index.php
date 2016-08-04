4dc050a61c3da1f0bf736fe2f9c5c235cbec52c9
Julian Andrade <julianchaos@gmail.com>
2016-08-04 16:45:18 -0300

    Configuração inicial da home e conteúdo do cabeçalho

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Beyonder.ME</title>
+		
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		
+		<link rel="stylesheet" href="assets/css/style.min.css" />
+    </head>
+    <body>
+		<div id="total">
+			<header>
+				<div class="container">
+					<div class="row">
+						<div class="col-xs-12">
+							<img src="assets/images/logo.png" alt="Beyonder.me logo" class="img-responsive" />
+						</div>
+						<div class="col-xs-12"><h1>Beyonder.ME frase de impacto</h1></div>
+						<div class="col-xs-12">
+							<a href="javascript: void(0)" class="btn btn-default btn-lg" role="button">
+								How It Works
+							</a>
+						</div>
+					</div>
+				</div>
+			</header>
+			<section id="main-content">
+				
+				<div div class="container">
+					<div class="row">
+						<div class="col-xs-12">
+							Formulário
+						</div>
+					</div>
+				</div>
+			</section>
+			<footer>
+				<div class="container">
+					<div class="row">
+						<div class="col-xs-12">
+							FOOTER
+						</div>
+					</div>
+				</div>
+			</footer>
+		</div>
+    </body>
+</html>
