581d146e875a1e2b46a8bc0c7728c508bd1b418a
Julian Andrade <julianchaos@gmail.com>
2016-08-29 23:40:18 -0300

    Código de acompanhamento do Google Analytics

--- app/templates/index.php
+++ app/templates/index.php
@@ -13,6 +13,16 @@
 		<script src="assets/bower_components/bootstrap/js/popover.js"></script>
 		<script src="assets/bower_components/OwlCarousel/owl-carousel/owl.carousel.min.js"></script>
 		<script src="assets/js/main.js"></script> 
+		
+		<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+			ga('create', 'UA-79742972-1', 'auto');
+			ga('send', 'pageview');
+		</script>
     </head>
     <body>
 		<div id="total">
